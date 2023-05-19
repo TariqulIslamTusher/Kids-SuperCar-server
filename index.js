b67f8e05baf5d4b9147395e9e9302c4bcbdf95a2
TariqulIslamTusher <islamtariqul652@gmail.com>
2023-05-19 12:25:01 +0600

    included the mongodb atlas and send the data to database through post

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ console.log(process.env.DB_PASS);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hskwi4h.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,7 +31,20 @@ const client = new MongoClient(uri, {
 const run =async()=> {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const toyDatabase = client.db("toysDB").collection('toys')
+
+    app.post('/addProduct', async (req, res)=>{
+        const newProduct = req.body
+        console.log(newProduct);
+        const result = await toyDatabase.insertOne(newProduct);
+        res.send(result)
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
