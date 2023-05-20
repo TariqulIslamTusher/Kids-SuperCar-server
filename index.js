f14dc97327fe0d6168e3efb37490482dfdcbb233
TariqulIslamTusher <islamtariqul652@gmail.com>
2023-05-20 12:29:51 +0600

    Patch file added and updated the file in database

--- index.js
+++ index.js
@@ -4,19 +4,14 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 
 // middleware
 app.use(cors())
 app.use(express.json())
 
-// env file
-console.log(process.env.DB_USER);  
-console.log(process.env.DB_PASS);  
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hskwi4h.mongodb.net/?retryWrites=true&w=majority`;
+
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,42 +23,73 @@ const client = new MongoClient(uri, {
   }
 });
 
-const run =async()=> {
+const run = async () => {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
     const toyDatabase = client.db("toysDB").collection('toys')
 
     // get all data from database
-    app.get('/products', async(req, res)=>{
-      const cursor =  toyDatabase.find()
+    app.get('/products', async (req, res) => {
+      const cursor = toyDatabase.find()
       const result = await cursor.toArray()
       res.send(result)
     })
 
-    // get only the data added by me 
-    app.get('/myAddedToys', async(req, res)=>{
-      const result = await toyDatabase.find().toArray()
+
+    //get data by query from the req
+    app.get('/addedProducts', async (req, res) => {
+      console.log(req.query);
+      let query = {}
+      if (req.query?.email) {
+        query = { sellerEmail: req.query.email }
+      }
+      const result = await toyDatabase.find(query).toArray()
       res.send(result)
     })
-
 
     // add product by a form in the database
-    app.post('/addProduct', async (req, res)=>{
-        const newProduct = req.body
-        console.log(newProduct);
-        const result = await toyDatabase.insertOne(newProduct);
-        res.send(result)
+    app.post('/addProduct', async (req, res) => {
+      const newProduct = req.body
+      const result = await toyDatabase.insertOne(newProduct);
+      res.send(result)
     })
 
 
-    // delete the products
-    app.delete('/products/:id', async(req,res)=>{
+    // get item by specific id
+    app.get('/products/:id', async (req, res) => {
       const id = req.params.id
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
+      const result = await toyDatabase.findOne(query)
+      res.send(result)
+    })
+
+    // delete the products
+    app.delete('/products/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
       const result = await toyDatabase.deleteOne(query)
       res.send(result)
+    })
+
+
+    //  Update items by using PUT or Patch for a single ID
+    app.patch('/products/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
+      const updatedProducts = req.body
+      const updateDoc = {
+        $set: {
+          price: updatedProducts.price,
+          availableQty: updatedProducts.qty,
+          description: updatedProducts.description
+        }
+      }
+      console.log(updatedProducts);
+
+      const result =await toyDatabase.updateMany(filter, updateDoc)
+      res.send(result)
+
     })
 
 
@@ -79,8 +105,8 @@ const run =async()=> {
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
-    res.send('sever site is running')
+app.get('/', (req, res) => {
+  res.send('sever site is running')
 })
 
 
@@ -92,6 +118,6 @@ app.get('/', (req, res)=>{
 
 
 // listen on the sever console
-app.listen(port, ()=>{
-    console.log(`server is running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`server is running on port : ${port}`);
 })
